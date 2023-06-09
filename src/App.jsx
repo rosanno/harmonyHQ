2707406ddf5c63a4b6a2c0038c1bfc7d3dc19ec7
rosanno <rosannomacabuhay@gmail.com>
2023-06-09 16:19:39 +0800

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-rose-700 underline font-semibold text-2xl">
+        HarmonyHQ
+      </h1>
+    </div>
+  );
+};
+
+export default App;
